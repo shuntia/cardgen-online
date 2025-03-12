19e488781e38a85365f0bc30e5fab5542862f314
shuntia <shunta@koga.us>
2025-03-12 10:29:44 -0700

    add advanced options

--- script.js
+++ script.js
@@ -22,3 +22,34 @@ function replacedesc(value) {
     }
     document.getElementById("description").innerHTML = accumulate;
 }
+
+function adjustbg(value){
+    var body=document.getElementById("body");
+    body.style.backgroundColor=value.value;
+}
+
+function togglemenu(){
+    var menu=document.getElementById("menu");
+    if(menu.style.display=="none"){
+        menu.style.display="flex";
+    }else{
+        menu.style.display="none";
+    }
+}
+
+function adjustheader(value){
+    var header=document.getElementById("header");
+    var genre=document.getElementById("genre");
+    header.style.backgroundColor=value.value;
+    genre.style.backgroundColor=value.value;
+}
+
+function descpad(value){
+    document.getElementById("description").style.padding=value.value+"px";
+}
+function descheight(value){
+    document.getElementById("description").style.height=value.value+"px";
+}
+function descwidth(value){
+    document.getElementById("description").style.width=value.value+"px";
+}
